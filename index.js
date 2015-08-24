4bcc23a0918ce325812d29460fe75bcbb91bb9c6
khejing <khejing3@aliyun.com>
2015-08-24 22:53:56 +0800

    webapp should use websocket

--- index.js
+++ index.js
@@ -5,9 +5,9 @@
 
 import forOwn from 'lodash/object/forOwn';
 let mqtt;
-if(DEVICE_TYPE === 'desktop'){
+if(DEVICE_TYPE === 'desktop' || MOBILE_TYPE === 'webapp'){
 	mqtt = require('mqtt');
-}else if(DEVICE_TYPE === 'mobile'){
+}else if(DEVICE_TYPE === 'mobile' && MOBILE_TYPE !== 'webapp'){
 	mqtt = cordova.require('cordova-plugin-mqtt-service.MQTTService');
 }
 
@@ -68,9 +68,9 @@ let mqttClient = {
                     console.log("unknown message!");
                 }
             };
-			if(DEVICE_TYPE === 'desktop'){
+			if(DEVICE_TYPE === 'desktop' || MOBILE_TYPE === 'webapp'){
 				mqttClientInstance.on('message', messageCb);
-			}else if(DEVICE_TYPE === 'mobile'){
+			}else if(DEVICE_TYPE === 'mobile' && MOBILE_TYPE !== 'webapp'){
 				mqtt.onMessage(messageCb);
 			}
             args.cb(LoginErrorCode.success);
@@ -95,30 +95,30 @@ let mqttClient = {
                 //setTimeout(function() { this.connect(); }, 200);
             //}
 		}
-		if(DEVICE_TYPE === 'desktop'){
+		if(DEVICE_TYPE === 'desktop' || MOBILE_TYPE === 'webapp'){
             mqttClientInstance = mqtt.connect(server, opts);
             mqttClientInstance.on('connect', sucessCb);
             this.onError(errorCb);
-		}else if(DEVICE_TYPE === 'mobile'){
+		}else if(DEVICE_TYPE === 'mobile' && MOBILE_TYPE !== 'webapp'){
 			mqtt.connect(server, opts, successCb, errorCb);
 		}
     },
     destroy: function(){
-		if(DEVICE_TYPE === 'desktop'){
+		if(DEVICE_TYPE === 'desktop' || MOBILE_TYPE === 'webapp'){
             if(mqttClientInstance){
                 mqttClientInstance.end();
                 mqttClientInstance = null;
             }
-		}else if(DEVICE_TYPE === 'mobile'){
+		}else if(DEVICE_TYPE === 'mobile' && MOBILE_TYPE !== 'webapp'){
 			mqtt.end();
 		}
         console.log("destroy mqtt client");
     },
     subscribe: function(topic){
         //TODO: {qos: 1}, make clear whether subscribe 0 and clean false won't receive old message
-		if(DEVICE_TYPE === 'desktop'){
+		if(DEVICE_TYPE === 'desktop' || MOBILE_TYPE === 'webapp'){
 			mqttClientInstance.subscribe(topic);
-		}else if(DEVICE_TYPE === 'mobile'){
+		}else if(DEVICE_TYPE === 'mobile' && MOBILE_TYPE !== 'webapp'){
 			mqtt.subscribe(topic);
 		}
     },
@@ -127,9 +127,9 @@ let mqttClient = {
 		let strToSend = JSON.stringify(object);
 		console.log("send to " + topic + ": " + strToSend);
         //TODO: {qos: 1}, make clear whether publish 0 and clean false won't receive old message by the other
-        if(DEVICE_TYPE === 'desktop'){
+        if(DEVICE_TYPE === 'desktop' || MOBILE_TYPE === 'webapp'){
 			mqttClientInstance.publish(topic, strToSend);
-		}else if(DEVICE_TYPE === 'mobile'){
+		}else if(DEVICE_TYPE === 'mobile' && MOBILE_TYPE !== 'webapp'){
 			mqtt.publish(topic, strToSend);
 		}
     },
@@ -157,9 +157,9 @@ let mqttClient = {
         }
     },
     onClose: function(cb){
-		if(DEVICE_TYPE === 'desktop'){
+		if(DEVICE_TYPE === 'desktop' || MOBILE_TYPE === 'webapp'){
 			mqttClientInstance.on('close', cb);
-		}else if(DEVICE_TYPE === 'mobile'){
+		}else if(DEVICE_TYPE === 'mobile' && MOBILE_TYPE !== 'webapp'){
 			mqtt.onConnectionLost(cb);
 		}
     },
