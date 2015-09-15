7806e99302c46d8561e68d0ad454d72aed3a6917
khejing <khejing@hotmail.com>
2015-09-15 20:00:19 +0800

    reset every time update callback has been called

--- index.js
+++ index.js
@@ -137,6 +137,11 @@ let mqttClient = {
                             console.log("main activity recevie message from background");
                             messageCb(ret.LatestResult.topic, ret.LatestResult.message);
                         }
+                        myService.deregisterForUpdates(function(ret){
+                            console.log("background service deregistering for updates: "+ret.RegisteredForUpdates);
+                        }, function(){
+                            console.log("background service deregistering for updates error");
+                        });
                         myService.registerForUpdates(updateCb, function(){
                             console.log("background service registering for updates error");
                         });
