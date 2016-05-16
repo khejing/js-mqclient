e2817bd0a99b8aa5bfa5d3807a7d0b2b39c09d61
khejing <khejing@hotmail.com>
2016-05-17 02:24:07 +0800

    比1.5稍大还是偶尔会出现重复登录，试试9秒

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ let mqClient = {
   connect: function(args){
     clientId = args.id;
     server = args.server;
-    let opts = {clean: args.cleanSession, clientId: clientId, keepalive: 5, reconnectPeriod: 8000};
+    let opts = {clean: args.cleanSession, clientId: clientId, keepalive: 5, reconnectPeriod: 9000};
     let errorCb = function(error){
       if(NETWORK_TYPE === 'websocket'){
         Logger.error("mqtt connect failed: "+error.message);
